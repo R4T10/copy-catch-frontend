1efa352c22011a14b104d6a5a4f6f2813b1c8cef
koonlookhin <krit_tipnuan@cmu.ac.th>
2023-07-24 16:51:00 +0700

    fixing in the detail in detail page

--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -1,11 +1,12 @@
 <template>
-  <div>
-    {{ GStore.compareDetail.name }}
+  <div v-for="data in filteredCompareData" :key="data">
+    {{ data.student_name }}
+    <br />
+    {{ data.answer }}
   </div>
-  <div
-    v-for="compare_data in GStore.compareDetail.copare_data"
-    :key="compare_data"
-  >
+  <h2>similar answer</h2>
+  <br />
+  <div v-for="compare_data in removeMatchingData" :key="compare_data">
     <span>
       {{ compare_data.student_name }} <br />
       {{ compare_data.answer }}
@@ -14,6 +15,22 @@
 </template>
 <script>
 export default {
-  inject: ['GStore']
+  inject: ['GStore'],
+  computed: {
+    filteredCompareData() {
+      console.log(this.GStore.compareDetail.copare_data)
+      return this.GStore.compareDetail.copare_data.filter(
+        (itemInArray) =>
+          itemInArray.student_name === this.GStore.compareDetail.name
+      )
+    },
+    removeMatchingData() {
+      const filteredData = this.GStore.compareDetail.copare_data.filter(
+        (itemInArray) =>
+          itemInArray.student_name !== this.GStore.compareDetail.name
+      )
+      return filteredData
+    }
+  }
 }
 </script>
