f176990a1e78f9bcc046a723b44a132f8eb736d3
whatsuplinlin <kjm010704@gmail.com>
2023-08-29 14:12:06 +0700

    Finish analysis page

--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -1,38 +1,40 @@
 <template>
-  <div>
+  <div class="heading">
     <h2>
       {{ GStore.compareDetail.question_number }}
       {{ GStore.compareDetail.question }}
     </h2>
-    <button @click="sendEmail">Send the email</button>
-    <div class="answerbox">
-      <h3>
-        {{ GStore.compareDetail.student_id }} {{ GStore.compareDetail.name }}
-      </h3>
-      <p>{{ GStore.compareDetail.answer }}</p>
-    </div>
-    <h2>Plagiarism Detected</h2>
-    <div v-if="GStore.compareDetail.check_table == 'student'">
-      <div
-        class="rankbox"
-        v-for="compare_data in GStore.compareDetail.compare_data"
-        :key="compare_data"
-      >
-        <span>
-          {{ compare_data.student_id }} <br />
-          {{ compare_data.student_name }} <br />
-          {{ compare_data.answer }}
-        </span>
+    <button @click="sendEmail">Send an email</button>
+  </div>
+  <div class="answerbox">
+    <h3>
+      {{ GStore.compareDetail.student_id }} {{ GStore.compareDetail.name }}
+    </h3>
+    <p>{{ GStore.compareDetail.answer }}</p>
+  </div>
+
+  <h2>Plagiarism Detected</h2>
+  <div v-if="GStore.compareDetail.check_table == 'student'">
+    <div
+      class="rankbox"
+      v-for="compare_data in GStore.compareDetail.compare_data"
+      :key="compare_data"
+    >
+      <div>
+        <h3>{{ compare_data.student_id }} {{ compare_data.student_name }}</h3>
+        <p>{{ compare_data.answer }}</p>
       </div>
     </div>
+  </div>
 
-    <div v-if="GStore.compareDetail.check_table == 'google'">
-      <div
-        v-for="compare_data in GStore.compareDetail.compare_data"
-        :key="compare_data"
-      >
-        <span> {{ compare_data.snippet }} </span>
-        <br />
+  <div v-if="GStore.compareDetail.check_table == 'google'">
+    <div
+      class="rankbox"
+      v-for="compare_data in GStore.compareDetail.compare_data"
+      :key="compare_data"
+    >
+      <div>
+        <h3>{{ compare_data.snippet }}</h3>
         <a :href="compare_data.link"> {{ compare_data.link }} </a>
       </div>
     </div>
@@ -67,7 +69,7 @@ export default {
               }
             })
             .catch((error) => {
-              Swal.fire('Error', 'Failed to Sending email', 'error')
+              Swal.fire('Error', 'Failed to send an email', 'error')
               console.error(error)
             })
         }
@@ -78,34 +80,60 @@ export default {
 </script>
 
 <style scoped>
+.heading {
+  display: flex; /* Display its children in a row */
+  align-items: center; /* Vertically align items */
+  justify-content: space-between;
+}
+
 h2 {
   text-align: left;
-  padding: 0 50px;
+  padding: 0 55px;
 }
 
 button {
-  display: block;
-  top: 0;
-  right: 0;
+  width: 150px;
+  height: 30px;
+  margin-right: 55px;
+  color: white;
+  background: #1c499e;
+  border: none;
+  font-weight: bold;
+  border-radius: 5px;
+  box-shadow: 1px 1px 1px 1px rgb(197, 193, 193);
+}
+
+button:hover {
+  box-shadow: 2px 2px 2px 2px rgb(197, 193, 193);
 }
 
 .answerbox {
   width: 90%;
   height: 200px;
-  border: 1px solid gray;
-  margin: auto;
   text-align: left;
+  margin: auto;
   padding: 0 20px;
   margin-bottom: 50px;
+  overflow-y: auto;
+  background: rgb(245, 250, 247);
+  box-shadow: 1px 1px 1px 1px rgb(197, 193, 193);
+  border-radius: 5px;
 }
 
 .rankbox {
   width: 90%;
-  height: 100px;
-  border: 1px solid gray;
-  margin: auto;
+  height: 150px;
   text-align: left;
+  margin: auto;
   padding: 0 20px;
-  margin-bottom: 30px;
+  margin-bottom: 20px;
+  overflow-y: auto;
+  background: rgb(245, 250, 247);
+  box-shadow: 1px 1px 1px 1px rgb(197, 193, 193);
+  border-radius: 5px;
+}
+
+a:hover {
+  color: #04724d;
 }
 </style>
